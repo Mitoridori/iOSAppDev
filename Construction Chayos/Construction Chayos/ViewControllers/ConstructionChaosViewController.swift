aeb97e38d9182cbcd5d06d4d3b08b1821f960a95
John Gotts <johngot@live.ca>
2019-06-12 15:36:55 -0400

    started to add storyboard elements
    started adding storyboard element for the level select screen

---
+++ Construction Chayos/Construction Chayos/ViewControllers/ConstructionChaosViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ConstructionChaosViewController.swift
+//  Construction Chayos
+//
+//  Created by john gotts on 2019-06-12.
+//  Copyright © 2019 Rebecca Stewart. All rights reserved.
+//
+
+import UIKit
+
+class ConstructionChaosViewController: UINavigationController {
+
+    override func viewDidLoad() {
+            super.viewDidLoad()
+            
+//            NotificationCenter.default.addObserver(self,
+//                                                   selector: #selector(showAuthenticationViewController),
+//                                                   name: NSNotification.Name(GameKitHelper.PresentAuthenticationViewController),
+//                                                   object: nil)
+//
+//            GameKitHelper.sharedInstance.authenticateLocalPlayer()
+        }
+        
+//        @objc func showAuthenticationViewController() {
+//            let gameKitHelper = GameKitHelper.sharedInstance
+//
+//            if let authenticationViewController =
+//                gameKitHelper.authenticationViewController {
+//                topViewController?.present(
+//                    authenticationViewController,
+//                    animated: true, completion: nil)
+//            }
+//        }
+//
+//        deinit {
+//            NotificationCenter.default.removeObserver(self)
+//        }
+}
